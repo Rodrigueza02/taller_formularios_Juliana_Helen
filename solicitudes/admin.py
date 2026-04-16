772c8caf7f11e518aa2c99bd245bfc251534fe43
HMAlejandra <helen.moncayo@campusucc.edu.co>
2026-04-16 17:35:39 -0500

    chore: eliminar app solicitudes para recrearla desde feature/solicitudes

--- solicitudes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Solicitud
-
-@admin.register(Solicitud)
-class SolicitudAdmin(admin.ModelAdmin):
-    list_display = ('nombre_solicitante', 'documento_identidad', 'tipo_solicitud', 'fecha_solicitud')
-    list_filter = ('tipo_solicitud', 'fecha_solicitud')
-    search_fields = ('nombre_solicitante', 'documento_identidad', 'asunto')
--- solicitudes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'solicitudes'
-
-urlpatterns = [
-    path('', views.registro_solicitud, name='formulario'),
-    path('confirmacion/', views.confirmacion_solicitud, name='confirmacion'),
-]
